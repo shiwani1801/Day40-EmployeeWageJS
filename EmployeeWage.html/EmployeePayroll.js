7f57215085a39c29674a1ce706f4c883286a6be4
shiwani <shiwanipaunikar1@gmail.com>
2023-03-06 01:38:35 -0800

    Added:Use Case11-Ability to check employee id and salary are non zero positive no. ,the gender is Male or Female and dat is not a future date

--- EmployeeWage.html/EmployeePayroll.js
+++ EmployeeWage.html/EmployeePayroll.js
@@ -1,6 +1,7 @@
 class EmployeePayrollData{
     //properties
     id;
+    name;
     salary;
     gender;
     startDate;
@@ -15,13 +16,37 @@ class EmployeePayrollData{
     }
 
     //getter and setter methods
-    get name() {return this._name;}
-    set name(name) {
+    get getId() {return this.id;}
+    get getName() {return this.name;}
+    get getSalary() {return this.salary;}
+    get getGender() {return this.gender;}
+    get getStartDate() {return this.startDate;}
+    set setId(id) {
+        let idRegex = RegExp("^[1-9]$");
+        if(idRegex.test(id)) this.id = id;
+        else throw 'ID is incorrect';
+    }
+    set setName(name) {
         let nameRegex = RegExp("^[A-Z]{1}[a-z]{3,}$");
-        if(nameRegex.test(name)) this._name = name;
+        if(nameRegex.test(name)) this.name = name;
         else throw 'Name is incorrect';
     }
-
+    set setSalary(salary) {
+        let salaryRegex = RegExp("^[1-9]$");
+        if(salaryRegex.test(salary)) this.salary = salary;
+        else throw 'Salary is incorrect';
+    }
+    set setGender(gender) {
+        let genderRegex = RegExp("^[M,F]{1}$");
+        if(genderRegex.test(gender)) this.gender = gender;
+        else throw 'Gender is incorrect';
+    }
+    set setStartDate(startDate) {
+        let startDateRegex = RegExp("^\\d[1980-2022]{4}-\\d[01-12]{2}-\\d[01-31]{2}$");
+        if(startDateRegex.test(startDate)) this.startDate = startDate;
+        else throw 'Start Date is incorrect';
+    }
+    
     //toString method
     toString(){
         const options={year:'numeric', month:'long', day:'numeric'};
@@ -35,11 +60,53 @@ class EmployeePayrollData{
 let employeePayrollData = new EmployeePayrollData(1,"Mark",3000,"M",new Date(2018,07,15));
 process.stdout.write(employeePayrollData.toString()+"\n");
 try{
-    employeePayrollData.name="Jon";
+    employeePayrollData.setName="Jon";
     process.stdout.write(employeePayrollData.toString()+"\n");
 }
 catch(exception){
     console.error(exception);
 }
 let employeePayrollData2=new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
-process.stdout.write(employeePayrollData2.toString());
+process.stdout.write(employeePayrollData2.toString()+"\n");
+try{
+    employeePayrollData2.setId=0;
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+}
+catch(exception){
+    console.error(exception);
+}
+try{
+    employeePayrollData2.setId=-1;
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+}
+catch(exception){
+    console.error(exception);
+}
+try{
+    employeePayrollData2.setSalary=-1;
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+}
+catch(exception){
+    console.error(exception);
+}
+try{
+    employeePayrollData2.setSalary=0;
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+}
+catch(exception){
+    console.error(exception);
+}
+try{
+    employeePayrollData2.setGender="G";
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+}
+catch(exception){
+    console.error(exception);
+}
+try{
+    employeePayrollData2.setStartDate=new Date(2023,01,01);
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+}
+catch(exception){
+    console.error(exception);
+}
