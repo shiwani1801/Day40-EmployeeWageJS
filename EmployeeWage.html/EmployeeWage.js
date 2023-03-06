fc105a11ab615489042e0e30abc1a5c6fecf5aaf
shiwani <shiwanipaunikar1@gmail.com>
2023-03-06 00:55:05 -0800

    Added:Use Case 4-Ability to calculate wage for month

--- EmployeeWage.html/EmployeeWage.js
+++ EmployeeWage.html/EmployeeWage.js
@@ -3,7 +3,6 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
 function getWorkingHours(empCheck) {
     switch(empCheck){
         case IS_PART_TIME:
@@ -15,8 +14,15 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp hours: "+empHrs);
+const NUM_OF_WORKING_DAYS = 20;
+
+let totalEmpHrs = 0;
+for(day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs = totalEmpHrs+getWorkingHours(empCheck);
+}
+
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+
+console.log("Emp hours: "+totalEmpHrs);
 console.log("Emp wage: "+empWage);
