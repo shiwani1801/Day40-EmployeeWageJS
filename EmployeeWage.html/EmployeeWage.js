41fb799d2cc7905956e3f6166052927c80375644
shiwani <shiwanipaunikar1@gmail.com>
2023-03-06 00:50:59 -0800

    Added:Use Case 3 Program - Refractor the code to write a function to get work hr

--- EmployeeWage.html/EmployeeWage.js
+++ EmployeeWage.html/EmployeeWage.js
@@ -3,17 +3,20 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10)%3 +1;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
+
+function getWorkingHours(empCheck) {
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp wage = "+empWage);
+console.log("Emp hours: "+empHrs);
+console.log("Emp wage: "+empWage);
